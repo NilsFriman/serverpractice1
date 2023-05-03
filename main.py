14b8ab845ee41428522ddbf4d44b28d3a7b349a0
litunaa <nubtem1337@gmail.com>
2023-05-03 23:19:36 +0200

    wallahi

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 import socket
 import pickle
+import threading
+import customtkinter
 
 
-print("your mom very hot")
+host = socket.gethostbyname(socket.gethostname())
+port = 52000
 
 
 
-print(b"amogo_logo")                                     
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen()
+
+clients = []
+names = []
+
+
+
+def messages(msg):
+    for client in clients:
+        client.send(msg)
+
+
+def client_handler(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            messages(message)
+
+        except Exception:
+            index_of_client = clients.index(client)
+            clients.remove(index_of_client)
+            client.close()
+            messages(f"{names[index_of_client]} Has left the chat".encode("utf-8"))
+            names.remove(clients[index_of_client])
+            break
+
+            
+            
