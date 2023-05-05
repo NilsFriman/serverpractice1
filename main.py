08346b0f794c0eefc60a31a21608452c3c4cba93
Litunaa <nubtem1337@gmail.com>
2023-05-05 09:13:31 +0200

    testest

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import socket
-import threading
-
-
-host = socket.gethostbyname(socket.gethostname())
-port = 52000
 
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen()
+s = socket.socket()
 
+s.connect(("10.158.18.101", 1234))
