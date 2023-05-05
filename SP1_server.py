9d68b9dc0ce8039d999df28de6988208b0ea1add
Litunaa <nubtem1337@gmail.com>
2023-05-05 08:55:23 +0200

    amogus

--- SP1_server.py
+++ SP1_server.py
@@ -1,6 +1,17 @@
 import socket
 import pickle
+import threading
 
-server_socket = socket.socket()
-host = socket.gethostname()
+
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostbyname(socket.gethostname())
 port = 1234
+server_socket.bind((host, port))
+server_socket.listen()
+
+
+def connection():
+    while True:
+        conn, addr = server_socket.accept()
